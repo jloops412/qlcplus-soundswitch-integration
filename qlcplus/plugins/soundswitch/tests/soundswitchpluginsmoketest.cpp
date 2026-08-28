9e8a89254e0d3388d9a649c2dfb2ae39836a9e30
J. Loops <66142072+jloops412@users.noreply.github.com>
2026-08-28 13:06:01 -0400

    Use the active integration name in source notices

--- qlcplus/plugins/soundswitch/tests/soundswitchpluginsmoketest.cpp
+++ qlcplus/plugins/soundswitch/tests/soundswitchpluginsmoketest.cpp
@@ -2,7 +2,7 @@
   Q Light Controller Plus
   soundswitchpluginsmoketest.cpp
 
-  Copyright (c) 2026 EmberLights contributors
+  Copyright (c) 2026 QLC+ SoundSwitch Integration contributors
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
