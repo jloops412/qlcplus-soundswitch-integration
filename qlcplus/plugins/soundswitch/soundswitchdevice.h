43a38b8c2d22315aa3c958bb66b1c95914a99399
J. Loops <66142072+jloops412@users.noreply.github.com>
2026-08-28 13:05:56 -0400

    Use the active integration name in source notices

--- qlcplus/plugins/soundswitch/soundswitchdevice.h
+++ qlcplus/plugins/soundswitch/soundswitchdevice.h
@@ -2,7 +2,7 @@
   Q Light Controller Plus
   soundswitchdevice.h
 
-  Copyright (c) 2026 EmberLights contributors
+  Copyright (c) 2026 QLC+ SoundSwitch Integration contributors
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
